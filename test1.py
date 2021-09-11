55f71f6784c6e1a324016d5922bb2c6cbea22fd4
Msmohits <88817177+Msmohits@users.noreply.github.com>
2021-09-11 15:04:34 +0530

    Asynchronization  in python
    Here to code is written for executing two loop at same time.

---
+++ test1.py
@@ -0,0 +1,37 @@
+import asyncio
+
+
+async def function_asyc():
+    for i in range(100000):
+        if i % 5000 == 0:
+            print("Hello, I'm Abhishek")
+            print("GFG is Great")
+
+            # New Line Added
+            await asyncio.sleep(0.01)
+    return 0
+
+
+async def function_2():
+    for i in range(10):
+
+        print("\n HELLO WORLD \n")
+        await asyncio.sleep(0.01)
+    return 0
+
+
+async def main():
+    f1 = loop.create_task(function_asyc())
+    f2 = loop.create_task(function_2())
+    await asyncio.wait([f1, f2])
+
+
+# to run the above function we'll
+# use Event Loops these are low level
+# functions to run async functions
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+loop.close()
+
+# You can also use High Level functions Like:
+# asyncio.run(function_asyc())
